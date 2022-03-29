730cc8f61bfbc2c359e4a40317504be7322905eb
Donelle Harris <donelle.harris@gmail.com>
2022-03-28 20:34:30 -0500

    Create components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,42 @@
 <script>
-	export let name;
+  import TopWaves from "./components/TopWaves.svelte";
+  import Header from "./components/Header.svelte";
+  import Aside from "./components/Aside.svelte";
+  import NewChit from "./components/NewChit.svelte";
+  import AllChits from "./components/AllChits.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div id="app-container" class="app-container">
+  <TopWaves />
+
+  <Header />
+
+  <section>
+    <div class="container">
+      <main>
+        <NewChit />
+        <AllChits />
+      </main>
+
+      <Aside />
+    </div>
+  </section>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  section .container {
+    display: flex;
+    flex-direction: row;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  section .container main {
+    flex-grow: 3;
+    flex-basis: 600px;
+    margin-left: 20px;
+  }
 </style>
